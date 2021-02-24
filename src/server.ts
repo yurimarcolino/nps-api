874eeb7e9448413c6d32a4359ed8e75a7dbb929c
Yuri <yurifmarcolino@gmail.com>
2021-02-23 21:03:00 -0300

    (feat): adding controllers, models, database

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message : "Hello World"})
-})
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Os dados foram salvos com sucesso!"});
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
-    console.log("Server is running at port 3333");
+    console.log("Server is running at port 3333!");
 });
